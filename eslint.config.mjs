e9131c4a0341b2b8a74382e0b22bef0710c591f2
Emerson Junior <emerson.c.junior.115@gmail.com>
2025-08-20 18:35:39 -0300

    fix: corrigindo eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,24 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from "@eslint/eslintrc"
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  // funciona a partir do Node 20.11.0
+  baseDirectory: import.meta.dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
+  ...compat.config({
+    extends: ["next", "next/core-web-vitals", "next/typescript"],
     rules: {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "@typescript-eslint/no-unused-vars": [
         "warn",
-        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
-      ]
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
     },
-  },
-];
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
